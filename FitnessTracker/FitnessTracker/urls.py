0ef11440b839fd5543453c4e1a809d78b93b7fd4
Steve Valles <savallesquiroz@mail.fhsu.edu>
2024-05-07 02:04:25 +0200

    Initial users app created with its initial html register and css

--- FitnessTracker/FitnessTracker/urls.py
+++ FitnessTracker/FitnessTracker/urls.py
@@ -5,6 +5,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('workouts/', include('Workouts.urls')),
+    path('users/', include('users.urls')),
 ]
 
 path('workouts/', include('Workouts.urls')),
---
+++ FitnessTracker/users/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import register_request
+
+urlpatterns = [
+    path("register/", register_request, name="register"),
+]
